aff3084d03ce11c5cd010f8376a217c595106eb2
vars416 <56270900+vars416@users.noreply.github.com>
2020-03-01 00:35:17 -0500

    Added components of Physics Game from Code-Lab 0

---
+++ CodeLab1-Week 3/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Controller : MonoBehaviour
+{
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemies")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("You Won");
+            Destroy(gameObject);
+            SceneManager.LoadScene("WinScreen", LoadSceneMode.Additive);
+        }
+    }
+}
